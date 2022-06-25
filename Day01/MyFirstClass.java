43670e72faa8d72b3b188b0d312e7cfa60c643ec
돌아온성덕 <shdnjs7104@naver.com>
2022-06-25 17:04:07 +0900

    집에서 커밋

--- Day01/MyFirstClass.java
+++ Day01/MyFirstClass.java
@@ -8,6 +8,7 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		System.out.println("My First Class"); //My First Class를 출력한다.
 		System.out.println("히읏히읏"); //히읏히읏을 출력한다.	
+		System.out.println("집에서 작업");
 	}
 
 }
