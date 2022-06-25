61d38627ef9fee305c92c6747c3ce181568d45c0
돌아온성덕 <shdnjs7104@naver.com>
2022-06-25 16:24:41 +0900

    첫 깃 커밋

---
+++ Day01/MyFirstClass.java
@@ -0,0 +1,13 @@
+/*
+  My Fisrt.java
+  Date:2022-06-25
+ */
+public class MyFirstClass {
+	
+	//프로그램 스타팅 포인트
+	public static void main(String[] args) {
+		System.out.println("My First Class"); //My First Class를 출력한다.
+		System.out.println("히읏히읏"); //히읏히읏을 출력한다.	
+	}
+
+}
